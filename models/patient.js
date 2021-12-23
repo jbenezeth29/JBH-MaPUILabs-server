d80329a4004c59a246772ad2544a16c94d9e2dc7
benezeth-j <benezeth-j@mgdis.fr>
2021-12-23 13:57:31 +0100

    link treatment to patient

--- models/patient.js
+++ models/patient.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose');
+const  mongoose = require('mongoose');
 
 const patientSchema = mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   age: { type: Number, required: true },
   sex: { type: Number, required: true },
+  treatments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref:"Treatment",
+    }
+  ]
 });
 
-module.exports = mongoose.model('Patient', patientSchema);
+module.exports = mongoose.model.call(require('mongoose'), 'Patient', patientSchema);
